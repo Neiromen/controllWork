2262c9efc4628485272bf96edf7198a0f97865e6
korobkoaiu.26 <alex.kor0702@gmail.com>
2025-12-09 10:21:57 +0300

    on 5

--- src/main/java/com/example/demo/MessageController.java
+++ src/main/java/com/example/demo/MessageController.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -16,6 +18,7 @@ public class MessageController {
     @PostMapping("")
     public ResponseEntity<Message> createMessage(@RequestBody Message message) {
         message.setId(messageList.size());
+        message.setCreatedAt(LocalDateTime.from(LocalDate.now()));
         messageList.add(message);
         return ResponseEntity.ok(message);
     }
@@ -31,37 +34,73 @@ public class MessageController {
     }
 
     @PutMapping("/{messageId}")
-    public ResponseEntity<Message> updateMessage(@PathVariable int messageId,@RequestBody Message message){
+    public ResponseEntity<Message> updateMessage(@PathVariable int messageId, @RequestBody Message message) {
         message.setId(messageId);
-        messageList.set(messageId,message);
+        messageList.set(messageId, message);
         return ResponseEntity.ok(message);
     }
 
     @DeleteMapping("/{messageId}")
-    public ResponseEntity<Void> deleteMessageById(@PathVariable int messageId){
+    public ResponseEntity<Void> deleteMessageById(@PathVariable int messageId) {
         messageList.remove(messageId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("")
-    public ResponseEntity<Void> deleteAllMessages(){
+    public ResponseEntity<Void> deleteAllMessages() {
         messageList.clear();
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/count")
-    public ResponseEntity<Integer> countMessagesLength(){
+    public ResponseEntity<Integer> countMessagesLength() {
         return ResponseEntity.ok(messageList.size());
     }
 
     @GetMapping("/author/{author}")
-    public ResponseEntity<List<Message>> getAllAuthorMessages(@PathVariable String author){
+    public ResponseEntity<List<Message>> getAllAuthorMessages(@PathVariable String author) {
         List<Message> authorMessages = new ArrayList<>();
-        for (Message message:messageList){
-            if (message.getAuthor().equals(author)){
+        for (Message message : messageList) {
+            if (message.getAuthor().equals(author)) {
                 authorMessages.add(message);
             }
         }
         return ResponseEntity.ok(authorMessages);
+    }
+
+    @GetMapping("/latest")
+    public ResponseEntity<List<Message>> getLastTwentyMessages() {
+        int endIndex = 19;
+        if (messageList.size() < 20) {
+            endIndex = messageList.size();
+        }
+        List<Message> messagesSortedByCreateTime = messageList.stream()
+                .sorted(Comparator.comparing(Message::getCreatedAt)).toList().reversed().subList(0, endIndex);
+        return ResponseEntity.ok(messagesSortedByCreateTime);
+    }
+
+    @GetMapping("/cheapest")
+    public ResponseEntity<List<Message>> getCheapest() {
+        List<Message> cheapest = messageList.stream().sorted(Comparator.comparing(Message::getPrice)).toList();
+        return ResponseEntity.ok(cheapest);
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<List<Message>> getTodayMessages() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Message> todayMessages = messageList.stream().
+                filter(message -> Duration.between(now, message.getCreatedAt()).toSeconds() < 86000).toList();
+        return ResponseEntity.ok(todayMessages);
+    }
+
+    @GetMapping("/search/{word}")
+    public ResponseEntity<List<Message>> searchMessagesByWord(@PathVariable String word) {
+        List<Message> foundedMessages = new ArrayList<>();
+        for (Message message:messageList){
+            if(message.getMessage().contains(word) || message.getTitle().contains(word)){
+                foundedMessages.add(message);
+            }
+        }
+        return ResponseEntity.ok(foundedMessages);
     }
 }
